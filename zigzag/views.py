f3af380cfc36363747c7475f4945dde7f4d685b2
Fruit Lee <glfruit80@gmail.com>
2017-01-07 00:54:52 +0800

    添加问卷显示页面

--- zigzag/views.py
+++ zigzag/views.py
@@ -19,10 +19,13 @@ def get_locale():
                     for translation in babel.list_translations()]
     return request.accept_languages.best_match(translations)
 
-#FIXME: 处理404错误时没有跳转到相应页面的问题
+# FIXME: 处理404错误时没有跳转到相应页面的问题
+
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
 
 @app.route('/')
 def index():
@@ -49,18 +52,21 @@ def upload():
     return render_template('upload.html')
 
 
+@app.route('/survey/', methods=['GET'])
+def survey_index():
+    return redirect(url_for('survey_list'))
+
+
 @app.route('/survey/create', methods=['GET', 'POST'])
 def survey_create():
     form = SurveyForm()
 
     if form.validate_on_submit():
-        app.logger.info("^^^^^^^^^^^^^^^^^^^^^^^^^^^valid survey data is acquired")
         survey = Survey()
         survey.title = request.form['title']
         survey.description = request.form['description']
         db.session.add(survey)
         db.session.commit()
-        app.logger.info("^^^^^^^^^^^^^^^^^^^^^^^^^^^valid survey data is saved")
         return redirect(url_for('survey_list'))
 
     if app.config['DEBUG']:
@@ -72,7 +78,13 @@ def survey_create():
 @app.route('/survey/list', methods=['GET'])
 def survey_list():
     surveys = Survey.query.all()
-    return render_template('survey/list.html', surveys = surveys)
+    return render_template('survey/list.html', surveys=surveys)
+
+
+@app.route('/survey/<survey_id>', methods=['GET'])
+def survey_show(survey_id):
+    survey = Survey.query.get(survey_id)  # FIXME:处理id不存在的情况
+    return render_template('survey/show.html', survey=survey)
 
 
 # NOTE: 来自http://flask.pocoo.org/snippets/12/，实现用flash存储表单错误信息的功能
